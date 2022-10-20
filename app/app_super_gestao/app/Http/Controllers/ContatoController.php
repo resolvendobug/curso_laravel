918f87db02b8d29a1cc4f576819f182cc0ae1a83
Renato Carvalho <renatocp@Renatos-MacBook-Pro.local>
2022-10-19 23:15:14 -0300

    Aprendendo Componente blade

--- app/app_super_gestao/app/Http/Controllers/ContatoController.php
+++ app/app_super_gestao/app/Http/Controllers/ContatoController.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 class ContatoController extends Controller
 {
     public function contato(){
+
+        echo var_dump($_POST);
         return view('site.contato');
     }
 }
