ea565b669417d483dff5d273a6a03c6f576d0f3c
Renato Carvalho <renatocp@Renatos-MacBook-Pro.local>
2022-11-20 00:13:20 -0300

    303. Upload de arquivos

--- app/app_locadora_carros/app/Http/Controllers/MarcaController.php
+++ app/app_locadora_carros/app/Http/Controllers/MarcaController.php
@@ -7,6 +7,12 @@ use Illuminate\Http\Request;
 
 class MarcaController extends Controller
 {
+
+    public function __construct(Marca $marca)
+    {
+        $this->marca = $marca;
+    }
+
     /**
      * Display a listing of the resource.
      *
@@ -15,6 +21,9 @@ class MarcaController extends Controller
     public function index()
     {
         //
+        //$marcas = Marca::all();
+        $marcas = $this->marca->all();
+        return $marcas;
     }
 
     /**
@@ -36,20 +45,29 @@ class MarcaController extends Controller
     public function store(Request $request)
     {
         //
-        $marca = Marca::create($request->all());
+        //  $marca = Marca::create($request->all());
+
+        $request->validate($this->marca->rules(), $this->marca->feedback());
+
+
+        $marca = $this->marca->create($request->all());
         return $marca;
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function show(Marca $marca)
+    public function show($id)
     {
         //
-        
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(['error' => 'Recurso pesquisado não existe'], 404);
+        }
+        return $marca;
     }
 
     /**
@@ -67,22 +85,50 @@ class MarcaController extends Controller
      * Update the specified resource in storage.
      *
      * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, Marca $marca)
+    public function update(Request $request, $id)
     {
         //
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(['error' => 'Recurso pesquisado não existe.'], 404);
+        }
+
+        if ($request->method() === 'PATCH') {
+            $regrasDinamicas = array();
+            //percorrer todas as regras
+            foreach ($marca->rules() as $input => $regra) {
+                //coletar apenas as regras aplicaveis aos parâmetros parciais da requisição PATCH
+                if (array_key_exists($input, $request->all())) {
+                    $regrasDinamicas[$input] = $regra;
+                }
+            }
+            $request->validate($regrasDinamicas, $marca->feedback());
+        } else {
+            $request->validate($marca->rules(), $marca->feedback());
+        }
+
+
+        $marca->update($request->all());
+        return $marca;
     }
 
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Models\Marca  $marca
+     * @param  Integer
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Marca $marca)
+    public function destroy($id)
     {
         //
+        $marca = $this->marca->find($id);
+        if ($marca === null) {
+            return response()->json(['error' => 'Recurso pesquisado não existe'], 404);
+        }
+        $marca->delete();
+        return ['msh' => 'Marca excluída com sucesso!'];
     }
 }
--- app/app_locadora_carros/app/Models/Marca.php
+++ app/app_locadora_carros/app/Models/Marca.php
@@ -8,5 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Marca extends Model
 {
     use HasFactory;
-    protected $fillable = ['nome','imagem'];
+    protected $fillable = ['nome', 'imagem'];
+
+    public function rules()
+    {
+        return [
+            'nome' => 'required|unique:marcas,nome,' . $this->id.'',
+            'imagem' => 'required'
+        ];
+    }
+
+    public function feedback()
+    {
+        return [
+            'required' => 'O campo :attribute deve ser preenchido',
+            'nome.unique' => 'Já existe uma marca com esse nome'
+        ];
+    }
 }
