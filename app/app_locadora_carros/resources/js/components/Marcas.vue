3d07670000efae748195d0bc0ebdcf5732e208ee
Renato Carvalho <renato@resolvendobug.com.br>
2022-12-04 22:22:26 -0300

    365 Recuperacao de registros de marcas

--- app/app_locadora_carros/resources/js/components/Marcas.vue
+++ app/app_locadora_carros/resources/js/components/Marcas.vue
@@ -52,26 +52,34 @@
 
         <!-- Modal -->
         <modal-component id="modalMarca" titulo="Adicionar marca">
+
+            <template v-slot:alertas>
+                <alert-component tipo="success" :detalhes="transacaoDetalhes" titulo="Cadastro realizado com sucesso." v-if="transacaoStatus == 'adicionado'"></alert-component>
+                <alert-component tipo="danger" :detalhes="transacaoDetalhes" titulo="Erro ao tentar cadastrar esta marca" v-if="transacaoStatus == 'erro'"></alert-component>
+            </template>
+
             <template v-slot:conteudo>
                 <div class="form-group">
                     <input-container-component titulo="Nome da marca" id="novoNome" id-help="novoNomeHelp"
                         texto-ajuda="Informe o nome da marca">
-                        <input type="text" class="form-control" id="inputNome" aria-describedby="novoNomeHelp"
-                            placeholder="Nome da marca">
+                        <input type="text" class="form-control" id="novoNome" aria-describedby="novoNomeHelp"
+                            v-model="nomeMarca" placeholder="Nome da marca">
                     </input-container-component>
+                    {{ nomeMarca }}
                 </div>
 
                 <div class="form-group">
                     <input-container-component titulo="Imagem" id="novoImagem" id-help="novoImagemHelp"
                         texto-ajuda="Selecione uma imagem">
                         <input type="file" class="form-control-file" id="novoImagem" aria-describedby="novoImagemHelp"
-                            placeholder="Seleciona uma imagem">
+                            placeholder="Seleciona uma imagem" @change="carregarImagem($event)">
                     </input-container-component>
+                    {{ arquivoImagem }}
                 </div>
             </template>
             <template v-slot:rodape>
                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fechar</button>
-                <button type="button" class="btn btn-primary">Salvar</button>
+                <button type="button" class="btn btn-primary" @click="salvar()">Salvar</button>
             </template>
 
         </modal-component>
@@ -81,5 +89,64 @@
 </template>
 
 <script>
+export default {
+    computed: {
+        token() {
+            let token = document.cookie.split(';').find(indice => {
+                return indice.includes('token=')
+            })
+            token = token.split('=')[1]
+            token = 'Bearer ' + token
+            return token
+        }
 
+    },
+    data() {
+        return {
+            urlbase: 'http://localhost/app_locadora_carros/public/api/v1/marca',
+            nomeMarca: '',
+            arquivoImagem: [],
+            transacaoStatus: '',
+            transacaoDetalhes: {}
+        }
+    },
+    methods: {
+        carregarImagem(e) {
+            this.arquivoImagem = e.target.files
+        },
+        salvar() {
+            console.log(this.nomeMarca)
+            console.log(this.arquivoImagem)
+            let formData = new FormData();
+            formData.append('nome', this.nomeMarca)
+            formData.append('imagem', this.arquivoImagem[0])
+
+            let config = {
+                headers: {
+                    'content-type': 'multipart/form-data',
+                    'Accept': 'application/json',
+                    'Authorization': this.token
+                }
+            }
+
+            axios.post(this.urlbase, formData, config)
+                .then(response => {
+                    this.transacaoStatus = 'adicionado'
+                    this.transacaoDetalhes = {
+                        mensagem:   'ID:'+response.data.id
+                    }
+                    console.log(response)
+                })
+                .catch(error => {
+                    this.transacaoStatus = 'erro'
+                    this.transacaoDetalhes = {
+                        mensagem:   'Erro:'+error.response.data.message,
+                        dados:   error.response.data.errors
+                    } 
+                    console.log(error.response.data.message)
+                })
+        }
+    }
+
+}
 </script>
